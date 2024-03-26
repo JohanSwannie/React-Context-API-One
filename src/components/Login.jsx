ddc2ddbf0827c4d780e8422cb0763e2f04944d28
Johan Swan <jswannieman@gmail.com>
2024-03-26 13:44:19 +1300

    Update Login JSX Component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,47 +3,60 @@ import UserContext from "../contexts/UserContext";
 
 function Login() {
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { setUser } = useContext(UserContext);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setUser({ username, password });
+    setUser({ username, email, password });
   };
 
   const clearFields = () => {
     setUsername("");
+    setEmail("");
     setPassword("");
   };
 
   return (
     <div>
       <h2>Login</h2>
-      <div className="form">
-        <div>
-          <input
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange={(event) => setUsername(event.target.value)}
-          />
+      <form onSubmit={handleSubmit}>
+        <div className="form">
+          <div>
+            <input
+              type="text"
+              value={username}
+              placeholder="Username"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              value={email}
+              placeholder="Email"
+              required
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+          <div>
+            <button onClick={clearFields}>Clear Fields</button>
+          </div>
         </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <div>
-          <button onClick={handleSubmit}>Submit</button>
-        </div>
-        <div>
-          <button onClick={clearFields}>Clear Fields</button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
