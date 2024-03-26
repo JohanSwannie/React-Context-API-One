856c177af9a24b11df7bc7c5a204ebdefd3c672c
Johan Swan <jswannieman@gmail.com>
2024-03-26 13:44:30 +1300

    Update Profile JSX Component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,12 +9,16 @@ function Profile() {
   if (user.username.length < 8)
     return <h1>User Name must be at least 8 Characters in length</h1>;
 
+  if (!user.email) return <h1>Email field must be filled in</h1>;
+
   if (user.password.length < 10)
     return <h1>Password must be at least 10 characters in length</h1>;
 
   return (
     <div>
-      <h2>Profile: {user.username}</h2>
+      <h2>
+        Profile: User : {user.username} --- Email : {user.email}
+      </h2>
     </div>
   );
 }
